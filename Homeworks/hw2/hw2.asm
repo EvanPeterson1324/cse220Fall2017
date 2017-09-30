8979a1d424140c6500c8aeae05a19034dde886f4
Evan <Evanp1324@gmail.com>
2017-09-30 12:14:23 -0400

    moved files around and init homework 2

---
+++ Homeworks/hw2/hw2.asm
@@ -0,0 +1,75 @@
+
+##############################################################
+# Homework #2
+# name: MY_NAME
+# sbuid: MY_SBU_ID
+##############################################################
+.text
+
+##############################
+# PART 1 FUNCTIONS
+##############################
+
+replace1st:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -555
+	############################################
+	jr $ra
+
+printStringArray:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -555
+	############################################
+    jr $ra
+
+verifyIPv4Checksum:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -555
+	############################################
+    jr $ra
+
+##############################
+# PART 2 FUNCTIONS
+##############################
+
+extractData:
+    #Define your code here
+	############################################
+	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+	li $v0, -555
+	li $v1, -555
+	############################################
+    jr $ra
+
+processDatagram:
+    #Define your code here
+
+    jr $ra
+
+##############################
+# PART 3 FUNCTIONS
+##############################
+
+printDatagram:
+    #Define your code here
+    ############################################
+    # DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
+    li $v0, -555
+    ############################################
+    jr $ra
+
+#################################################################
+# Student defined data section
+#################################################################
+.data
+.align 2  # Align next items to word boundary
+
+#place all data declarations here
+
+
